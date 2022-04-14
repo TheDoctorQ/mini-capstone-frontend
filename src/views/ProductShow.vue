274524f18d494d3d47bbb6a11ca235a177692e34
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-04-13 23:07:31 -0600

    adds ProductShow view

---
+++ src/views/ProductShow.vue
@@ -0,0 +1,49 @@
+<script>import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Product Show",
+      product: {}
+    };
+  },
+  created: function () {
+    this.showProduct();
+  },
+  methods: {
+    showProduct: function () {
+      axios.get(`/products/${this.$route.params.id}.json`)
+        .then(response => {
+          console.log(response.data);
+          this.product = response.data;
+        })
+    },
+    destroyProduct: function (product) {
+      axios.delete(`/products/${product.id}.json`).
+        then(response => {
+          console.log(response.data);
+          this.$router.push('/products')
+        })
+    },
+    editProduct: function (product) {
+      this.$router.push(`/products/${product.id}/edit`);
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="products-show">
+    <h1>{{ message }}</h1>
+    <img v-for="image in product.images" v-bind:src="image.url" style="max-width: 85%">
+    <h2>{{ product.name }}</h2>
+    <p>{{ product.description }}</p>
+    <p>Price: ${{ product.price }}</p>
+    <p>Tax: ${{ product.tax }}</p>
+    <p>Total: ${{ product.total }}</p>
+    <button v-on:click="destroyProduct(product)">Delete Product</button>
+    <button v-on:click="editProduct(product)">Edit Product</button>
+  </div>
+</template>
+
+<style>
+</style>
